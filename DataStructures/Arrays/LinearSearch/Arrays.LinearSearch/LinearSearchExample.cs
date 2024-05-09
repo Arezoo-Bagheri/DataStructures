6a34748edb93d1e92c23648a4a0655be727ee31f
Arezo Bagheri <ArezoBagheri@outlook.com>
2024-05-09 11:45:48 +0330

    Add Arrays.LinearSearch

---
+++ DataStructures/Arrays/LinearSearch/Arrays.LinearSearch/LinearSearchExample.cs
@@ -0,0 +1,16 @@
+﻿namespace Arrays.LinearSearch
+{
+    public class LinearSearchExample
+    {
+        public static int LinearSearch(int[] arra, int target)
+        {
+            for (int i = 0; i < arra.Length; i++)
+            {
+                if (arra[i] == target)
+                    return i;
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ DataStructures/Arrays/LinearSearch/Arrays.LinearSearch/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Arrays.LinearSearch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 4, 10, 40, 5, 7 };
+            int taget = 5;
+            int result = LinearSearchExample.LinearSearch(array, taget);
+
+            if (result != -1)
+                Console.WriteLine("Element is present at index : " + result);
+
+            else
+                Console.WriteLine("Element is not present in array");
+        }
+
+    }
+}
